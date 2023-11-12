9f101eb6b37afaecf098da4fbf49595a23c6c484
Ahigherlove38 <7237@holbertonstudents.com>
2023-11-12 02:25:00 -0800

    lists.h update

--- more_singly_linked_lists/remove_spaces.sh
+++ more_singly_linked_lists/remove_spaces.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name "*.c" -exec sed -i 's/^[[:space:]]*//; s/[[:space:]]*$//' {} +
+find . -type f -name "*.c" -exec sed -i 's/[[:space:]]\{2,\}/ /g; s/^[[:space:]]*//; s/[[:space:]]*$//' {} +
