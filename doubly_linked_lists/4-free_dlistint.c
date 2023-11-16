d1cdb924975936f38bd88672b5326948f365f7e4
Ahigherlove38 <7237@holbertonstudents.com>
2023-11-16 10:36:38 -0800

    6-sum_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * free_dlistint - frees a dlistint_t list.
- * @head: pointer to head of the list
- * Return: nothing
- **/
+* free_dlistint - frees a dlistint_t list.
+* @head: pointer to head of the list
+* Return: nothing
+**/
 void free_dlistint(dlistint_t *head)
 {
-	if (head == NULL)
-	return;
+if (head == NULL)
+return;
 
-	while (head->next)
-	{
-	head = head->next;
-	free(head->prev);
-	}
-	free(head);
+while (head->next)
+{
+head = head->next;
+free(head->prev);
+}
+free(head);
 }
